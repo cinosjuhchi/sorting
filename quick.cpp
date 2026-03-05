b7a8066300c2bc1d23fc7295187a0768dd07f1b8
Muhammad Habibullah Mursalin <pacino447@gmail.com>
2026-03-05 16:27:28 +0700

    refactor: streamline quick sort implementation and remove unused code

--- quick.cpp
+++ quick.cpp
@@ -1,76 +1,56 @@
 #include <iostream>
+
 using namespace std;
 
-void partition(int *nums, int size, int *mainPivot) {
-    int pivot = *(nums + size - 1);
-    int i = -1;
-    while(i < size)
-    {
-        for (int j = 0; j < size; j++)
-        {            
-            if (*(nums + j) < pivot)
-            {
-                i += 1;
-                int temp = *(nums+j);
-                *(nums+j) = *(nums+i);
-                *(nums+i) = temp;
-            }
-            
-        }
-        i += 1;
-        int temp = *(nums+i);
-        *(nums+i) = pivot;
-        *(nums+size-1) = temp;        
-        *mainPivot = i;
-        break;
-    }
-    
-        
+
+void swapElements(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void quickSort(int *nums, int index, int pivot, int size) {
-    if(index > pivot && index < size) {        
-        quickSort(nums, index+1, pivot, size);
-        if (*(nums + index) > *(nums + index + 1))
+int partition(int *nums, int low, int high) {
+    int pivot = *(nums + high);
+    int i = (low-1);
+    for (int j = low; j < high; j++)
+    {
+        if (*(nums+j) < pivot )
         {
-            int temp = *(nums + index);
-            *(nums + index) = *(nums + index + 1);
-            *(nums + index + 1) = temp; 
-            quickSort(nums, index+1, pivot, size);
-        }                                    
-    }else if(index < pivot && index > 0) {
-        quickSort(nums, index-1, pivot, size);
-        if (*(nums + index) < *(nums + index - 1))
-        {
-            int temp = *(nums + index);
-            *(nums + index) = *(nums + index - 1);
-            *(nums + index - 1) = temp;                        
-            quickSort(nums, index-1, pivot, size);
+            i++;
+            swapElements((nums+j), (nums+i));
         }
-    }else {
-        return;
-    }    
-    
+        
+    }
+    swapElements((nums+i+1), (nums+high));
+    return i+1;
+}
+
+void quickSort(int *nums, int low, int high) {
+    if(low < high) {
+        int pivot = partition(nums, low, high);
+
+        quickSort(nums, low, (pivot-1));
+        quickSort(nums, (pivot+1), high);
+    }
 }
 
 int main() {
-    int size = 9;
-    int mainPivot = 0;
-    int nums[size] = {24, 46, 11, 26, 38, 57, 27, 20, 25};
-    int *p = nums;    
-    partition(p, size, &mainPivot);
-    cout <<endl;        
-    // cout << mainPivot <<endl;        
-    quickSort(p, mainPivot+1, mainPivot, size);
-    quickSort(p, mainPivot-1, mainPivot, size);
-    for (int i = 0; i < (size); i++)
-    {        
-        if(i == (size - 1)) {
-            cout << nums[i];
-        } else {
-            cout << nums[i] << ", ";
+    int size = 8;
+    int arr[] = {90, 34, 27, 45, 120, 54, 95, 65};
+
+    quickSort(arr, 0, (size - 1));
+
+
+    for (int i = 0; i < size; i++)
+    {
+        if (i == (size - 1))
+        {
+            cout << arr[i];
+        }else {
+            cout << arr[i] << ",";
         }
+        
     }
-    return 0;
+    
 
 }
